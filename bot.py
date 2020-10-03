851170e3bef69f0412fe0fdd0799b22113611f1d
Julian Heidt <julian.heidt@gmail.com>
2020-10-02 21:54:16 -0400

    Needs work on intergrating both APIs

--- bot.py
+++ bot.py
@@ -2,25 +2,36 @@ import schoolopy
 import yaml
 import webbrowser as wb
 import discord
+import json
 from discord.ext import commands
 
-#with open('config.yml', 'r') as f:
- #   cfg = yaml.load(f)
+f = open(r'D:\\Microsoft VS Code\\Projects\\Random Python Shit\\Discord Bot for school\\config.yml')
+cfg = yaml.load(f, Loader=yaml.FullLoader)
 
-sc = schoolopy.Schoology(schoolopy.Auth('a09fd862af22d4f1d928d79788567eaf05f7682cc', 'c02d5c0b937de605e20691f12af9c213f'))
-sc.limit = 10  # Only retrieve 10 objects max
+sc = schoolopy.Schoology(schoolopy.Auth(cfg['key'], cfg['secret']))
 
-print(sc.get_buildings('butler tech'))
+#print('Your name is %s' % sc.get_me().name_display)
+#print(sc.get_assignment(section_id=2733685453, assignment_id=2897150593), output)
+assignment = sc.get_assignment(section_id=2733685453, assignment_id=2897150593)
+#assignment = json.loads(x)
+print(assignment.title, assignment.due)
 
-client = commands.Bot(command_prefix= '~')
+client = commands.Bot(command_prefix= '/')
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
     
     
-@client.event
+@client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}')
+
+@client.command()
+async def assignment(ctx):
+    global assignment
+    await ctx.send(f'Here is an assignment {assignment.title, assignment.due}')
+
+
 
 client.run('NzYxMzk3MDI1NjMwNzE1OTE2.X3aAPA.jPt5ndp91qFz4M9vkBdg4IyAKVY')
